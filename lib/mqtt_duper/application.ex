7b7d5ce4090ba2ba67a1e4e6209eba339be30925
Christophe De Troyer <christophe.detroyer@gmail.com>
2023-10-02 20:34:14 +0200

    wip

--- lib/mqtt_duper/application.ex
+++ lib/mqtt_duper/application.ex
@@ -8,11 +8,9 @@ defmodule MqttDuper.Application do
   @impl true
   @spec start(any, any) :: {:error, any} | {:ok, pid}
 
-  IO.puts """
-  Root directory: #{:code.root_dir()}
-  Config file: #{File.read!(:code.root_dir() <> "/mqtt_endpoints.exs"))}
-  """
   def start(_type, _args) do
+    debug_info()
+
     children = [
       {MqttDuper.Listener,
        [
@@ -35,5 +33,58 @@ defmodule MqttDuper.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_all, name: MqttDuper.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp debug_info do
+    root_dir = :code.root_dir()
+    config_path = Path.join(:code.root_dir(), "/mqtt_endpoints.exs")
+    config_exists? = File.exists?(config_path)
+
+    IO.puts("""
+    Root directory: #{root_dir}
+    Config file: #{if config_exists?, do: File.read!(config_path), else: "#{config_path} not found"}
+    """)
+
+    # source certs
+    source = Application.get_env(:mqtt_duper, :source)
+
+    source
+    |> Keyword.get(:ssl_opts, [])
+    |> Keyword.get(:cacertfile, "not defined")
+    |> tap(&IO.puts("source cacertfile: #{&1}"))
+    |> tap(fn file -> file |> File.read!() |> IO.puts() end)
+
+    source
+    |> Keyword.get(:ssl_opts, [])
+    |> Keyword.get(:certfile, "not defined")
+    |> tap(&IO.puts("source certfile: #{&1}"))
+    |> tap(fn file -> file |> File.read!() |> IO.puts() end)
+
+    source
+    |> Keyword.get(:ssl_opts, [])
+    |> Keyword.get(:keyfile, "not defined")
+    |> tap(&IO.puts("source keyfile: #{&1}"))
+    |> tap(fn file -> file |> File.read!() |> IO.puts() end)
+
+    # destination certs
+    destination = Application.get_env(:mqtt_duper, :source)
+
+    destination
+    |> Keyword.get(:ssl_opts, [])
+    |> Keyword.get(:cacertfile, "not defined")
+    |> tap(&IO.puts("destination cacertfile: #{&1}"))
+    |> tap(fn file -> file |> File.read!() |> IO.puts() end)
+
+    destination
+    |> Keyword.get(:ssl_opts, [])
+    |> Keyword.get(:certfile, "not defined")
+    |> tap(&IO.puts("destination certfile: #{&1}"))
+    |> tap(fn file -> file |> File.read!() |> IO.puts() end)
+
+    destination
+    |> Keyword.get(:ssl_opts, [])
+    |> Keyword.get(:keyfile, "not defined")
+    |> tap(&IO.puts("destination keyfile: #{&1}"))
+    |> tap(fn file -> file |> File.read!() |> IO.puts() end)
   end
 end
