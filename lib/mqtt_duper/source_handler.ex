a95aed267d9e2617d894f142be9f31d637998747
Christophe De Troyer <christophe.detroyer@gmail.com>
2023-10-14 10:07:01 +0200

    warnings and config

--- lib/mqtt_duper/source_handler.ex
+++ lib/mqtt_duper/source_handler.ex
@@ -24,30 +24,52 @@ defmodule MqttDuper.SourceHandler do
   end
 
   #############################################################################
-  # Helpers
+  # Transforming
 
+  # @doc """
+  # Transforms the topic of the message by applying all availble transformers.
+  # """
   @spec transform_topic(publish_message) :: publish_message
   defp transform_topic(message) do
     :mqtt_duper
     |> Application.get_env(:transformers, [])
     |> Keyword.get(:topics, [])
-    |> Enum.reduce(message, fn {regex, replacement}, message ->
-      case message do
-        {:publish, m} ->
-          transformed = Map.update!(m, :topic, fn topic -> Regex.replace(regex, topic, replacement) end)
-          {:publish, transformed}
-
-        _ ->
-          message
-      end
-    end)
+    |> Enum.reduce(message, &apply_transformer/2)
   end
 
+  @doc """
+  Applies a transformer to the given message.
+  """
+  @spec apply_transformer({Regex.t(), String.t()}, publish_message | any) :: publish_message | any
+  defp apply_transformer({regex, replacement}, {:publish, message}) do
+    case message do
+      {:publish, m} ->
+        transformed = Map.update!(m, :topic, fn topic -> Regex.replace(regex, topic, replacement) end)
+        {:publish, transformed}
+
+      _ ->
+        message
+    end
+  end
+
+  defp apply_transformer(_transformer, message) do
+    message
+  end
+
+  #############################################################################
+  # Forwarding
+
+  # @doc """
+  # Checks if the message must be forwarded.
+  # """
   @spec forward?(publish_message) :: true | false
   defp forward?(m) do
     forward_topic?(m)
   end
 
+  # @doc """
+  # Checks if the topic of the given message is eligible for forwarding.
+  # """
   @spec forward_topic?(publish_message) :: true | false
   defp forward_topic?({:publish, %{topic: topic}}) do
     :mqtt_duper
@@ -56,6 +78,9 @@ defmodule MqttDuper.SourceHandler do
     |> Enum.any?(fn topic_filter -> Regex.match?(topic_filter, topic) end)
   end
 
+  # @doc """
+  # Forwards the message on the MQTT endpoint.
+  # """
   @spec forward_message(publish_message) :: :ok | {:error, term()}
   defp forward_message({:publish, %{topic: topic, payload: payload}}) do
     case Process.whereis(:emqtt_destination) do
